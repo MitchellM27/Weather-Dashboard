de98baac37792fee806f3592c829d401129712e5
Mitchell <mitchellm27@gmail.com>
2022-01-28 00:49:51 -0800

    updated js to reflect forecast

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ init();
 //Storing the list of cities
 function storeCityList(){
    localStorage.setItem("citiesList", JSON.stringify(citiesList));
-   console.log(localStorage);
  }  
 
 //adding input cities to the list
@@ -40,22 +39,35 @@ searchButton.on("click", function(event) {
     var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=imperial';
     var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid='+ apiKey;
 
-    console.log(weatherURL)
 
     fetch(weatherURL)
     .then(response => {
         return response.json();
     }).then (data => {
         console.log(data)
+        var conditions = (data.weather['0'].main)
         var lat = (data.coord.lat)
         var long = (data.coord.lon)
 
         var today = moment();
+        var iconToday = $("<i>")
+        var currentD =$("#currentDay")
         $("#currentWeather").attr("class", "card");
         $("#currentDay").text(city + ": " + today.format("MMM Do, YYYY"));
         $("#temp").text("Temp: " + data.main.temp + "°F");
         $("#wind").text("Wind: " + data.wind.speed + "MPH");
         $("#humidity").text("Humidity: " + data.main.humidity + " %");
+        currentD.append(iconToday);
+
+        if (conditions === "Clouds") {
+            iconToday.attr("class", "fa-cloud ")
+        }else if (conditions === "Clear") {
+            iconToday.attr("class", "fas fas-sun")
+        }else {
+            iconToday.attr("class", "fas fa-cloud-rain")
+        }
+
+
 
     }).catch (err => {
         console.error(err);
@@ -66,13 +78,31 @@ searchButton.on("click", function(event) {
         return response.json();
     }).then (data => {
         console.log(data)
-        console.log(data.list[0]["dt_txt"])
         for (i=0; i<5; i++) {
+            var forecastIcon = $("</i>")
+            var forecastLoopint = ("#forecastDay"+i)
+            var forecastD = $(forecastLoopint)
             $("#forecast").attr("class", "card");
-            $("#forecastDay"+i).text(city + ": " + data.list[(i+1)*8]["dt_txt"]);
-            $("#temp"+i).text("Temp: " + data.list[(i+1)*8].main.temp);
-            $("#wind"+i).text("Wind: " + data.list[(i+1)*8].wind.speed);
-            $("#humidity"+i).text("Humidity: " + data.list[(i+1)*8].main.humidity);
+            $("#forecastDay"+i).text(city + ": " + data.list[(i)*8]["dt_txt"]);
+            //convert temp from Kelvin
+            fTemp= parseInt(((data.list[(i)*8].main.temp)-273)* (9/5) +32);
+            $("#temp"+i).text("Temp: " + fTemp + "°F");
+            $("#wind"+i).text("Wind: " + data.list[(i)*8].wind.speed + "MPH");
+            $("#humidity"+i).text("Humidity: " + data.list[(i)*8].main.humidity + " %");
+
+            forecastConditions = (data.list[(i)*8].weather["0"]["main"])
+
+            forecastD.append(forecastIcon)
+
+            if (forecastConditions === "Clouds") {
+                forecastIcon.attr("class", "fa-cloud ")
+            }else if (forecastConditions === "Clear") {
+                forecastIcon.attr("class", "fas fas-sun")
+            }else {
+                forecastIcon.attr("class", "fas fa-cloud-rain")
+            }
+
+            
         }
     }).catch (err => {
         console.error(err);
@@ -91,20 +121,7 @@ function renderCitiesEl() {
       li.attr("id","listEl");
       li.attr("data-city", city);
       li.attr("class", "list-group-item");
-      console.log(li);
       citiesListEl.prepend(li);
     }
 
 }   
-
-
-
-
-// fetch(weatherURL)
-//     .then(response => {
-//         return response.json();
-//     }).then (data => {
-//         console.log(data)
-//     }).catch (err => {
-//         console.error(err);
-//     });
